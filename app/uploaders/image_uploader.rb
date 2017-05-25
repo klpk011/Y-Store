61c27c874424dc67b4f4b731ee8bbd3cfaa6d602
klpk011 <klpk011@klpk011.local>
2017-05-25 10:24:52 +0800

    fix image upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,9 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   # storage :fog
-   storage :qiniu
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -25,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
    end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  # def default_url(*args)
   #   # For Rails 3.1 asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/"  [version_name, "default.png"].compact.join('_'))
   #
