b13f0ea2aeae9ae39724c958e641e066c90a9f25
klpk011 <klpk011@klpk011.local>
2017-06-01 17:25:34 +0800

    edit category

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 User.create(email: "admin@gmail.com", password: "123456", password_confirmation: "123456", is_admin: true)
+
+# 新增類型  Category #
+  CategoryGroup.create!(
+    name: 'aaa'
+  )
+
+  CategoryGroup.create!(
+    name: 'bbb'
+  )
+
+  CategoryGroup.create!(
+    name: 'ccc'
+  )
+
+
+# 新增分類  Category #
+  # aaa
+  # 1
+  Category.create!(
+    category_group_id: 1,
+    name: 'a1'
+  )
+
+  # 2
+  Category.create!(
+    category_group_id: 1,
+    name: 'a2'
+  )
+
+  # 3
+  Category.create!(
+    category_group_id: 1,
+    name: 'a3'
+  )
+
+  # bbb
+  # 4
+  Category.create!(
+    category_group_id: 2,
+    name: 'b1'
+  )
+
+  # 5
+  Category.create!(
+    category_group_id: 2,
+    name: 'b2'
+  )
+
+  # 6
+  Category.create!(
+    category_group_id: 2,
+    name: 'b3'
+  )
+
+
+  # ccc
+  # 7
+  Category.create!(
+    category_group_id: 3,
+    name: 'c1'
+  )
+  # 8
+  Category.create!(
+    category_group_id: 3,
+    name: 'c2'
+  )
+  # 9
+  Category.create!(
+    category_group_id: 3,
+    name: 'c3'
+  )
