496d50592f5714053c905007d5876d41c559b4d6
klpk011 <klpk011@klpk011.local>
2017-06-08 22:10:18 +0800

    update category

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,75 +1,75 @@
 User.create(email: "admin@gmail.com", password: "123456", password_confirmation: "123456", is_admin: true)
 
 # 新增類型  Category #
+CategoryGroup.create!(
+  name: ' 英语实用课程',
+  description: '爱上英语，快乐使用英语'
+)
   CategoryGroup.create!(
-    name: 'aaa'
+    name: '元认知系列课程',
+    description: "开启学习元认知"
   )
 
   CategoryGroup.create!(
-    name: 'bbb'
+    name: '考试英语',
+    description: '提升实力，得到高分'
   )
 
-  CategoryGroup.create!(
-    name: 'ccc'
-  )
+
 
 
 # 新增分類  Category #
-  # aaa
-  # 1
-  Category.create!(
-    category_group_id: 1,
-    name: 'a1'
-  )
-
-  # 2
-  Category.create!(
-    category_group_id: 1,
-    name: 'a2'
-  )
-
-  # 3
-  Category.create!(
-    category_group_id: 1,
-    name: 'a3'
-  )
-
-  # bbb
+# 1
+Category.create!(
+  category_group_id: 1,
+  name: '妈咪爹地宝贝'
+)
+# 2
+Category.create!(
+  category_group_id: 1,
+  name: '分分钟开口说英语'
+)
+# 3
+Category.create!(
+  category_group_id: 1,
+  name: '旅行英语口语'
+)
   # 4
   Category.create!(
     category_group_id: 2,
-    name: 'b1'
+    name: '英语学习元认知'
   )
 
   # 5
   Category.create!(
     category_group_id: 2,
-    name: 'b2'
+    name: '技能英语'
   )
 
   # 6
   Category.create!(
     category_group_id: 2,
-    name: 'b3'
+    name: '商务英语'
   )
 
-
-  # ccc
   # 7
   Category.create!(
     category_group_id: 3,
-    name: 'c1'
+    name: '中考英语'
   )
+
   # 8
   Category.create!(
     category_group_id: 3,
-    name: 'c2'
+    name: '高考英语'
   )
+
   # 9
   Category.create!(
     category_group_id: 3,
-    name: 'c3'
+    name: '托福'
   )
+
 
   Product.create!(
         name: '英语“使用”元认知
@@ -108,7 +108,7 @@ User.create(email: "admin@gmail.com", password: "123456", password_confirmation:
  ',
         description: '让你说出英国话。
  ',
-        category_id: 1,
+        category_id: 2,
         Applicants: '1、善于思考。
 2、不会说英语，不想被市面上的各种课程花式欺骗的想学真东西的同学。
 3、希望能够掌握英语日常基础会话的同学。 ',
@@ -175,7 +175,7 @@ Product.create!(
  ',
       description: '让你说英语时越来越自信，有种骑着摩托车在路上奔驰的兴奋感。
  ',
-      category_id: 3,
+      category_id: 2,
       Applicants: '1、善于思考。
 2、不会说英语，不想被市面上的各种课程花式欺骗的想学真东西的同学。
 3、希望能够掌握英语日常基础会话的同学。
@@ -205,10 +205,10 @@ Product.create!(
 )
 
 Product.create!(
-      name: '编程英语 ',
+      name: '编程英语',
       description: '让你再也不用因为英语拦路而学不好编程。
  ',
-      category_id: 4,
+      category_id: 5,
       Applicants: '想要学编程，但是英语0基础的“预备程序员”
  ',
       teaching_objectives: '1、让你记住编程所需要了解的200个常用单词。
@@ -240,7 +240,7 @@ Product.create!(
  ',
       description: '给你的美国之行增添“谈”资，让你更加享受你的旅行。
  ',
-      category_id: 5,
+      category_id: 3,
       Applicants: '1、一个月后要去美国旅行的同学
 2、害怕英语影响旅行感觉的同学 ',
       teaching_objectives: '让你会说旅行中最需要用到的话 ',
@@ -268,7 +268,7 @@ Product.create!(
       name: '英国旅行英语口语 ',
       description: '给你的美国之行增添“谈”资，让你更加享受你的旅行。
  ',
-      category_id: 5,
+      category_id: 3,
       Applicants: '1、一个月后要去英国旅行的同学
 2、不想让英语影响旅行感觉的同学 ',
       teaching_objectives: '让你会说旅行中最需要用到的话
@@ -323,4 +323,108 @@ Product.create!(
  ',
       service: '课后答疑
  '
+)
+Product.create!(
+      name: 'Peppa and her family（一） ',
+      description: '快乐了解应该文化，以及家庭日常英语口语 ',
+      category_id: 1,
+      Applicants: '3岁-6岁孩童以及他们的父母 ',
+      teaching_objectives: '掌握家庭日常英语口语 ',
+      Course_Contents: '了解英国家庭文化，学会日常英语口语 ',
+      course_features: '让孩子成长，开口讲出地道英文 ',
+      curriculum: 20,
+      Teacher_introduction: 'Claire
+1、专注心理学知识和英语教学的结合5年，将最前沿的心理学成果融入教学，有自己独特的教学理念；
+2、发音地道，英文水平高，但那都不算什么，那是最基本的条件；
+3、最重要的是：Claire老师最擅长激发学生的学习兴趣。
+4、跟她学，你很有可能会爱上英文的，因为她已经促成很多这样的奇妙反应。^_^ ',
+      price: 40,
+      quantity: 1999,
+      class_schedule: '7：00--8：00pm ',
+      class_location: '在线--一块儿听听 ',
+      service: '线下团体交流 '
+)
+
+Product.create!(
+      name: 'Peppa and her family（二）',
+      description: '让孩子成长，开口讲出地道英文 ',
+      category_id: 1,
+      Applicants: '3岁-6岁孩童以及他们的父母 ',
+      teaching_objectives: '掌握家庭日常英语口语 ',
+      Course_Contents: '了解英国家庭文化，学会日常英语口语 ',
+      course_features: '让孩子成长，开口讲出地道英文 ',
+      curriculum: 20,
+      Teacher_introduction: 'Claire
+1、专注心理学知识和英语教学的结合5年，将最前沿的心理学成果融入教学，有自己独特的教学理念；
+2、发音地道，英文水平高，但那都不算什么，那是最基本的条件；
+3、最重要的是：Claire老师最擅长激发学生的学习兴趣。
+4、跟她学，你很有可能会爱上英文的，因为她已经促成很多这样的奇妙反应。^_^ ',
+      price: 40,
+      quantity: 1999,
+      class_schedule: '7：00--8：00pm ',
+      class_location: '在线--一块儿听听 ',
+      service: '线下团体交流 '
+)
+
+Product.create!(
+      name: 'Peppa and her family（三）',
+      description: '快乐了解英国家庭文化，以及家庭日常英语口语 ',
+      category_id: 1,
+      Applicants: '3岁-6岁孩童以及他们的父母 ',
+      teaching_objectives: '让孩子成长，开口讲出地道英文 ',
+      Course_Contents: '了解英国家庭文化，学会日常英语口语 ',
+      course_features: '在于加入的互动中学会英语 ',
+      curriculum: 20,
+      Teacher_introduction: 'Claire
+1、专注心理学知识和英语教学的结合5年，将最前沿的心理学成果融入教学，有自己独特的教学理念；
+2、发音地道，英文水平高，但那都不算什么，那是最基本的条件；
+3、最重要的是：Claire老师最擅长激发学生的学习兴趣。
+4、跟她学，你很有可能会爱上英文的，因为她已经促成很多这样的奇妙反应。^_^ ',
+      price: 40,
+      quantity: 1999,
+      class_schedule: '7：00--8：00pm ',
+      class_location: '在线--一块儿听听 ',
+      service: '线下团体交流 '
+)
+
+Product.create!(
+      name: 'Peppa and her family（四）',
+      description: '快乐了解英国家庭文化，以及家庭日常英语口语 ',
+      category_id: 1,
+      Applicants: '3岁-6岁孩童以及他们的父母 ',
+      teaching_objectives: '掌握家庭日常英语口语 ',
+      Course_Contents: '了解英国家庭文化，学会日常英语口语 ',
+      course_features: '让孩子成长，开口讲出地道英文 ',
+      curriculum: 20,
+      Teacher_introduction: 'Claire
+1、专注心理学知识和英语教学的结合5年，将最前沿的心理学成果融入教学，有自己独特的教学理念；
+2、发音地道，英文水平高，但那都不算什么，那是最基本的条件；
+3、最重要的是：Claire老师最擅长激发学生的学习兴趣。
+4、跟她学，你很有可能会爱上英文的，因为她已经促成很多这样的奇妙反应。^_^ ',
+      price: 40,
+      quantity: 1999,
+      class_schedule: '7：00--8：00pm ',
+      class_location: '在线--一块儿听听 ',
+      service: '线下团体交流 '
+)
+
+Product.create!(
+      name: 'Peppa and her friends(一)',
+      description: '让孩子学会与朋友相处 ',
+      category_id: 1,
+      Applicants: '3-12岁儿童 ',
+      teaching_objectives: '让孩子学会友好与他人相处，用英语好好说话 ',
+      Course_Contents: '孩子之间的日常用语 ',
+      course_features: '让孩子学会与人相处 ',
+      curriculum: 20,
+      Teacher_introduction: 'Claire
+1、专注心理学知识和英语教学的结合5年，将最前沿的心理学成果融入教学，有自己独特的教学理念；
+2、发音地道，英文水平高，但那都不算什么，那是最基本的条件；
+3、最重要的是：Claire老师最擅长激发学生的学习兴趣。
+4、跟她学，你很有可能会爱上英文的，因为她已经促成很多这样的奇妙反应。^_^ ',
+      price: 40,
+      quantity: 1999,
+      class_schedule: '7：00--8：00pm ',
+      class_location: '在线--一块儿听听 ',
+      service: '线下团体交流 '
 )
